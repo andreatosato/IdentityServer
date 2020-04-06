ef80539f30b50f89b8bc2c9ea47570a54a448a6d
Andrea Tosato <andrea.tosato@hotmail.it>
2020-04-06 20:46:20 +0200

    Creation

---
+++ src/is4admin/Custom/AuthorizationCodeReceived.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace is4admin.Custom
+{
+	public static class AuthorizationCodeReceived
+	{
+		public static async Task CodeRedemptionAsync(AuthorizationCodeReceivedContext ctx)
+		{
+			var id_token = ctx.ProtocolMessage.IdToken;
+			var code = ctx.ProtocolMessage.Code;
+			string access_token = "";
+
+			ctx.HandleCodeRedemption(access_token, id_token);
+		}
+	}
+}
